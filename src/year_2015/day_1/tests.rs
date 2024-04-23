843e3f08a2e0f5daf888957245f5f2eaec902a80
RobinToncourt <robin.toncourt@gmail.com>
2024-04-23 22:39:16 +0200

    Remplacement des 4 espaces par des tabulations.

--- src/year_2015/day_1/tests.rs
+++ src/year_2015/day_1/tests.rs
@@ -1,41 +1,41 @@
 #[cfg(test)]
 mod tests {
-    use crate::utils::utils::load_from_file;
-    use super::super::solutions::{which_floor, position_enter_basement};
-    
-    #[test]
-    fn test_which_floor_examples() {
-        assert_eq!(which_floor("(())"), 0);
-        assert_eq!(which_floor("()()"), 0);
-        
-        assert_eq!(which_floor("((("), 3);
-        assert_eq!(which_floor("(()(()("), 3);
-        assert_eq!(which_floor("))((((("), 3);
-        
-        assert_eq!(which_floor("())"), -1);
-        assert_eq!(which_floor("))("), -1);
-        
-        assert_eq!(which_floor(")))"), -3);
-        assert_eq!(which_floor(")())())"), -3);
-    }
+	use crate::utils::utils::load_from_file;
+	use super::super::solutions::{which_floor, position_enter_basement};
+	
+	#[test]
+	fn test_which_floor_examples() {
+		assert_eq!(which_floor("(())"), 0);
+		assert_eq!(which_floor("()()"), 0);
+		
+		assert_eq!(which_floor("((("), 3);
+		assert_eq!(which_floor("(()(()("), 3);
+		assert_eq!(which_floor("))((((("), 3);
+		
+		assert_eq!(which_floor("())"), -1);
+		assert_eq!(which_floor("))("), -1);
+		
+		assert_eq!(which_floor(")))"), -3);
+		assert_eq!(which_floor(")())())"), -3);
+	}
 
-    #[test]
-    fn test_which_floor_puzzle_inputs() {
-        let inputs: Vec<String> =
+	#[test]
+	fn test_which_floor_puzzle_inputs() {
+		let inputs: Vec<String> =
 			load_from_file("./src/year_2015/day_1/inputs.txt");
-        assert_eq!(which_floor(inputs.get(0).unwrap().as_str()), 280);
-    }
-    
-    #[test]
-    fn test_position_enter_basement_examples() {
-        assert_eq!(position_enter_basement(")"), 1);
-        assert_eq!(position_enter_basement("()())"), 5);
-    }
+		assert_eq!(which_floor(inputs.get(0).unwrap().as_str()), 280);
+	}
+	
+	#[test]
+	fn test_position_enter_basement_examples() {
+		assert_eq!(position_enter_basement(")"), 1);
+		assert_eq!(position_enter_basement("()())"), 5);
+	}
 
-    #[test]
-    fn test_position_enter_basement_puzzle_inputs() {
-        let inputs: Vec<String> =
+	#[test]
+	fn test_position_enter_basement_puzzle_inputs() {
+		let inputs: Vec<String> =
 			load_from_file("./src/year_2015/day_1/inputs.txt");
-        assert_eq!(position_enter_basement(inputs.get(0).unwrap().as_str()), 1797);
-    }
+		assert_eq!(position_enter_basement(inputs.get(0).unwrap().as_str()), 1797);
+	}
 }
