49dcc2bc35d02d0737654f4d1d9e266fd771300f
RobinToncourt <robin.toncourt@gmail.com>
2024-04-23 20:18:31 +0200

    Correction suite à la migration.

--- src/year_2015/day_3/tests.rs
+++ src/year_2015/day_3/tests.rs
@@ -1,7 +1,10 @@
 #[cfg(test)]
-pub mod tests_day_3 {
-    use crate::utils::load_from_file;
-    use crate::day_3::{at_least_one_present, at_least_one_present_with_robo_santa};
+pub mod tests {
+    use crate::utils::utils::load_from_file;
+    use super::super::solutions::{
+		at_least_one_present,
+		at_least_one_present_with_robo_santa
+	};
     
     #[test]
     fn test_at_least_one_present_examples() {
@@ -12,7 +15,8 @@ pub mod tests_day_3 {
     
     #[test]
     fn test_at_least_one_present_puzzle_inputs() {
-        let inputs: Vec<String> = load_from_file("./src/year_2015/day_3/day_3_inputs.txt");
+        let inputs: Vec<String> =
+			load_from_file("./src/year_2015/day_3/inputs.txt");
         assert_eq!(at_least_one_present(inputs.get(0).unwrap()), 2565);
     }
     
@@ -25,7 +29,11 @@ pub mod tests_day_3 {
     
     #[test]
     fn test_at_least_one_present_with_robo_santa_puzzle_inputs() {
-        let inputs: Vec<String> = load_from_file("./src/year_2015/day_3/day_3_inputs.txt");
-        assert_eq!(at_least_one_present_with_robo_santa(inputs.get(0).unwrap()), 2639);
+        let inputs: Vec<String> =
+			load_from_file("./src/year_2015/day_3/inputs.txt");
+        assert_eq!(
+			at_least_one_present_with_robo_santa(inputs.get(0).unwrap()),
+			2639
+		);
     }
 }
